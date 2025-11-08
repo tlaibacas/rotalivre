4b8878bbb402ef9996b211111c8a332ac492618e
Tiago Abreu Laibacas <tiago.abreu.laibacas@gmail.com>
2025-11-08 17:10:54 +0000

    🔥 wipe: everything was deleted due to chaos — sometimes it's better to restart and learn again

--- src/app/[locale]/(main)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function MainLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      {children}
-    </div>
-  );
-}
